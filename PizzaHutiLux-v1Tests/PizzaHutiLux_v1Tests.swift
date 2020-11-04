0aacb77a64949fbab019a3db2de4856ef359d72c
Joshua <whatahaa@gmail.com>
2020-11-04 11:08:30 -0700

    Initial Commit

---
+++ PizzaHutiLux-v1Tests/PizzaHutiLux_v1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  PizzaHutiLux_v1Tests.swift
+//  PizzaHutiLux-v1Tests
+//
+//  Created by Joshua Marriott on 11/4/20.
+//
+
+import XCTest
+@testable import PizzaHutiLux_v1
+
+class PizzaHutiLux_v1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
